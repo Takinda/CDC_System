7a7dfa6fd2ce49005e2379f37623f130e3f9cb0f
Mortda Jabbar Hassan <mc.mortda@gmail.com>
2024-07-24 12:21:30 +0300

    Finish Drug Store

--- src/DrugStore/DrugStore.jsx
+++ src/DrugStore/DrugStore.jsx
@@ -1,4 +1,6 @@
-import MainImage from "../../Assets/images/DrugStoreMain.png"
+import MainImage from './MainImage.png'
+import "./DrugStore.css"
+import MedicationTable from "../pages/Pharmcy/MedicationTable.jsx"
 function DrugStore()
 {
 return <>
@@ -7,11 +9,19 @@ return <>
 <h1>Drug Store</h1>
 <img className="Main-Image" src={MainImage} alt="Drug Store Main Image" />
 </nav>
-<div>
-
+<div className="Boxes-Container">
+                    <div className="Search-box">
+                        <i className="fa-search" aria-hidden="true"></i>
+                        <input type="text" name="" placeholder="Search...?" />
+                    </div>
+                    <div className="AddNew-box">
+                        <i className="fa-plus" aria-hidden="true"></i>
+                        <button className="Add-Btn">Add</button>
+                    </div>
+ </div>
+<div className="Table-Container">
+<MedicationTable />
 </div>
-
-
 
 </>
 
