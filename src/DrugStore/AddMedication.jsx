69c1f2da5de005856d5483aa1b37c7c92b7cb5b5
Mortda Jabbar Hassan <mc.mortda@gmail.com>
2024-07-25 01:28:12 +0300

    something will be deleted it is for test

---
+++ src/DrugStore/AddMedication.jsx
@@ -0,0 +1,16 @@
+import './AddMedication.css'
+import MainImage from './DrugImage.png'
+function AddMedication()
+{
+return 
+<>
+<nav>
+<h1>Add Medication</h1>
+<img className="Main-Image" src={MainImage} alt="Drug Store Main Image" />
+</nav>
+
+</>
+
+}
+
+export default AddMedication;
