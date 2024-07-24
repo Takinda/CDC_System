01557bbd4216c24b521705f6177873ed853acf0c
Mortda Jabbar Hassan <mc.mortda@gmail.com>
2024-07-25 01:58:15 +0300

    test

--- src/DrugStore/AddMedication.jsx
+++ src/DrugStore/AddMedication.jsx
@@ -2,10 +2,21 @@ import './AddMedication.css'
 import MainImage from './DrugImage.png'
 function AddMedication()
 {
-return 
-<>
-<h1>test</h1>
-</>
+return <>
+<nav>
+<h1>Add Medication</h1>
+<img className="Main-Image" src={MainImage} alt="Drug Store Main Image" />
+</nav>
+
+
+<div className="form-container">
+<form>
+   <div className='input-box'><label htmlFor="">Name:</label>  <input name="Name" type="text" /></div> 
+   </form>
+
+</div>
+ 
+</> 
 
 };
 
