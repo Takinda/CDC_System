9e6dc2a088372a34324fc7d89d00bb3286cbddfa
Mortda Jabbar Hassan <mc.mortda@gmail.com>
2024-07-25 02:18:34 +0300

    test

--- src/DrugStore/AddMedication.jsx
+++ src/DrugStore/AddMedication.jsx
@@ -11,8 +11,16 @@ return <>
 
 <div className="form-container">
 <form>
-   <div className='input-box'><label htmlFor="">Name:</label>  <input name="Name" type="text" /></div> 
-   </form>
+   <div className='my-input-box'><label htmlFor="">Name:</label>  <input name="Name" type="text" /></div> 
+   <div className='my-input-box'><label htmlFor="">Count:</label>  <input name="Count" type="text" /></div> 
+   <div className='my-input-box'><label htmlFor="">Expiry:</label>  <input name="Expiry" type="text" /></div> 
+   <div className='my-input-box'><label htmlFor="">Who Added:</label>  <input name="WhoAdded" type="text" /></div> 
+   <div className='my-input-box'><label htmlFor="">Price:</label>  <input name="Price" type="text" /></div> 
+   <div className='my-input-box'><label htmlFor="">Entry Date:</label>  <input name="EntryDate" type="text" /></div> 
+   <div className='my-input-box'><label htmlFor="">Batch Number:</label>  <input name="BatchNumber" type="text" /></div> 
+   <div className='my-input-box'><label htmlFor="">MOH Number:</label>  <input name="MOHNumber" type="text" /></div>
+   <div className='my-input-box'><label htmlFor="">Note:</label>  <textarea name="Note" id="note-text"></textarea></div> 
+    </form>
 
 </div>
  
