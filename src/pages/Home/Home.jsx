338f5f4f206c844c175d84b45b9038226281e969
Takinda <gghanimalokaily@gmail.com>
2024-07-28 16:35:32 +0300

    test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Home.css'; // استيراد ملف CSS المخصص للأنماط
-import homeLogo from '../Assets/images/Health.png'; // استيراد صورة شعار الصفحة الرئيسية
-import iraqiLogo from '../Assets/images/Iraqi.png'; // استيراد صورة شعار 
+import homeLogo from '../../Assets/images/Health.png'; // استيراد صورة شعار الصفحة الرئيسية
+import iraqiLogo from '../../Assets/images/Iraqi.png'; // استيراد صورة شعار 
 
 const Home = () => {
   return (
